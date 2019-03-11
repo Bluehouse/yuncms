161bf874456c35a54f607b36f7b5a91ac35b2b23
Bluehouse <nie_yunfei@163.com>
2019-03-11 23:09:16 +0800

    前台集成sentry日志收集组件

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -27,9 +27,32 @@ return [
             // this is the name of the session cookie used for login on the frontend
             'name' => 'advanced-frontend',
         ],
+        // 配置sentry日志收集
+        'sentry' => [
+            'class' => 'mito\sentry\Component',
+            'dsn' => 'https://402bb2239c354b46b87bca06ca366999:4a06fd45290e46f5867e0eb25302ad7c@sentry.io/1412540', // private DSN
+//            'publicDsn' => 'https://402bb2239c354b46b87bca06ca366999@sentry.io/1412540', // 收集js错误时候打开
+            'environment' => 'staging', // if not set, the default is `production`
+            'jsNotifier' => true, // to collect JS errors. Default value is `false`
+            'jsOptions' => [ // raven-js config parameter
+                'whitelistUrls' => [ // collect JS errors from these urls
+                    // 收集所有的URL的
+//                    'http://staging.my-product.com',
+//                    'https://my-product.com',
+                ],
+            ],
+        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
+            // 新增sentry配置
             'targets' => [
+                [
+                    'class' => 'mito\sentry\Target',
+                    'levels' => ['error', 'warning'],
+                    'except' => [
+                        'yii\web\HttpException:404', // 除了404异常不收集
+                    ],
+                ],
                 [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -2,10 +2,13 @@
 
 namespace frontend\controllers;
 
+use Yii;
+
 class IndexController extends CommonController
 {
     public function actionIndex() {
 //        $this->layout = false; // 不是用公共layout
+        Yii::warning('this is a test warning');
         $this->layout = "layout";
         return $this->render("index");
     }
